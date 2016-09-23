f36771334758ee81b2b11baf916839b52cd0dbe9
Ron Lin <ronrlin@gmail.com>
2016-09-23 12:23:04 -0400

    trying a db write

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.get('/', function(request, response) {
 
 app.get('/realgraph/listen', function(request, response) {
 	request.query.url
-	var queryString = "INSERT INTO realgraph_pings(url, hash) VALUES (" + request.query.url + ", md5(" + request.query.url +"))";
+	var queryString = "INSERT INTO realgraph_pings(url, hash) VALUES ($1, md5($1))";
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query(queryString, function(err, result) {
+		client.query(queryString, [request.query.url], function(err, result) {
 			done();
 			if (err)
 				{ console.error(err); response.send("Error " + err); }

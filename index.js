f823272813184b37f9533eaff333c6860e14feeb
Krzysztof Skarupa <krzysztof.skarupa@gmail.com>
2016-11-10 16:12:25 +0100

    Created the actual `allowCrossDomain` function

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 app.use(allowCrossDomain);
 
 var pg = require('pg');

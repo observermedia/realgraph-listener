52acf58b1fe0897946cea401ef2b346352a5f867
Ron Lin <ronrlin@gmail.com>
2016-11-10 09:55:09 -0500

    https for jquery

--- beacon/beacon.js
+++ beacon/beacon.js
@@ -1,7 +1,7 @@
 (function(global) {
     var scriptName = "beacon.js"; //name of this script, used to get reference to own tag
     var jQuery; //noconflict reference to jquery
-    var jqueryPath = "http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js";
+    var jqueryPath = "https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js";
     var jqueryVersion = "1.8.3";
     var scriptTag; //reference to the html script tag
 
